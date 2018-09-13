0ae2b0ebd3ca45626c886dea8d9fd3ecd743ceba
Katrinhh <katrine.hveding@hotmail.com>
2018-09-13 09:12:00 +0200

    Added components folder

---
+++ src/components/TestComponent.js
@@ -0,0 +1,11 @@
+import React, {Component } from 'react'
+
+class TestComponent extends Component {
+
+    render() {
+        return <h3> Hello hello!</h3>;
+    };
+
+};
+
+export default TestComponent;
