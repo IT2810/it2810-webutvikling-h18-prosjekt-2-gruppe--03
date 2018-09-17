05112e85b5c3a71e35a34cc5ae66132d3cc28572
Dabble <ddabble@users.noreply.github.com>
2018-09-17 11:23:44 +0200

    Cleanup

--- src/App.js
+++ src/App.js
@@ -1,27 +1,26 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import HeaderComponent from './components/HeaderComponent';
 import TextComponent from './components/TextComponent';
 import logo from './logo.svg';
 import './App.css';
 
 
-
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <HeaderComponent />
-        <TextComponent />
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h1 className="App-title">Welcome to React</h1>
+                </header>
+                <p className="App-intro">
+                    To get started, edit <code>src/App.js</code> and save to reload.
+                </p>
+                <HeaderComponent />
+                <TextComponent />
+            </div>
+        );
+    }
 }
 
 export default App;
