73b39ce89e7d928639fcf60d8631e04db0733bdd
MiriamFi <mfinjord@gmail.com>
2018-09-17 10:09:33 +0200

    Added the last text files

---
+++ public/media/text/cats/text4.js
@@ -0,0 +1,7 @@
+{
+  "title": "",
+  "author": "Kobayashi Issa",
+  "text": "Arise from sleep, old cat\nand with great yawns and stretchings\namble out for love",
+  "collection": "",
+  "source": "https://letterpile.com/poetry/haiku-poems-about-cats"
+}
---
+++ public/media/text/chocolate/text4.js
@@ -0,0 +1,7 @@
+{
+  "title": "",
+  "author": "Mary Gay Kearns",
+  "text": "Chocolate squash and hurricane.\nCome toddle here your hands stretched out\nWith chocolate mouse and lemon squash\nYou are my candy, sugar babe\nArrived at forty in a hurricane \nBut if love can spin a web\nYou little darling got in my head.\n\nLove Grandma xxxx",
+  "collection": "",
+  "source": "https://hellopoetry.com/words/chocolate/"
+}
