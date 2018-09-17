37b43480d25f3edc5e511e297c0db71b39c1136a
MiriamFi <mfinjord@gmail.com>
2018-09-17 11:12:50 +0200

    Added temporary text component

--- src/App.js
+++ src/App.js
@@ -17,8 +17,8 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        //<HeaderComponent />
-        //<TextComponent />
+        <HeaderComponent />
+        <TextComponent />
       </div>
     );
   }
---
+++ src/components/TextComponent.js
@@ -0,0 +1,13 @@
+import React, {Component } from 'react'
+
+class TextComponent extends React.Component{
+  render(){
+    return(
+      <div className="textComp">
+      <p>The text goes here</p>
+      </div>
+    );
+  }
+}
+
+export default TextComponent;
