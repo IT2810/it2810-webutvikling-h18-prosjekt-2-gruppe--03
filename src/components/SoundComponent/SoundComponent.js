92f3c843ac64c19c6946eb0d0f8693279c316d1e
Dabble <ddabble@users.noreply.github.com>
2018-09-19 17:56:33 +0200

    SoundComponent gets file name
    ...from category and file number. #7

--- src/components/SoundComponent/SoundComponent.js
+++ src/components/SoundComponent/SoundComponent.js
@@ -1,18 +1,36 @@
 import React, {Component} from 'react';
-//import '../App.css';
-
 
 class SoundComponent extends Component {
+    category;
+    fileNumber;
+
     render() {
         return (
-            <div className="Sound">
+            <div className="SoundComponent">
                 <audio controls>
-                    <source src="/media/sound/animals/cat-stevens.mp3" type="audio/mpeg" />
-                    Your browser does not support the audio element.
+                    <source src={`/media/sound/${this.props.category}/${getFileName(this.props.category, this.props.fileNumber)}`} type="audio/mpeg" />
+                    Your browser does not support HTML5 audio.
                 </audio>
+                <p>
+                    <b>{this.props.category}{this.props.fileNumber}:</b>
+                    {getFileName(this.props.category, this.props.fileNumber)}
+                </p>
             </div>
         );
     }
+}
+
+const fileDict = {
+    "animals": ["6-cat-meows.mp3", "cat-stevens.mp3", "labrador.mp3", "lion.mp3"],
+    "food": ["apple-bite.mp3", "eat-chips.mp3", "smack-lips.mp3", "straw-slurp.mp3"],
+    "footsteps": ["footsteps.mp3", "footsteps-on-cement.mp3", "hard-shoes.mp3", "hiking-a-trail.mp3"]
+};
+
+function getFileName(category, fileNumber) {
+    if (category === undefined || category === "")
+        return; // TODO:
+
+    return fileDict[category][fileNumber - 1];
 }
 
 export default SoundComponent;
