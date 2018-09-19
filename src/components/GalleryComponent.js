7078341875628f06f01f25a5aa5312132ed35c54
Dabble <ddabble@users.noreply.github.com>
2018-09-19 17:02:25 +0200

    Whitespace cleanup

--- src/components/GalleryComponent.js
+++ src/components/GalleryComponent.js
@@ -1,47 +1,48 @@
 import React, {Component} from 'react'
 import CategoryComponent from './CategoryComponent';
 import TabComponent from "./TabComponent"
+
 //import MediaComponent from "./MediaComponent"
 
 class GalleryComponent extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      activeTab:1, imageCategory:"", textCategory:"", soundCategory:"",
-    };
-    this.getActiveTab = this.getActiveTab.bind(this);
-    this.getCategories = this.getCategories.bind(this);
-  }
+    constructor() {
+        super();
+        this.state = {
+            activeTab: 1, imageCategory: "", textCategory: "", soundCategory: "",
+        };
+        this.getActiveTab = this.getActiveTab.bind(this);
+        this.getCategories = this.getCategories.bind(this);
+    }
 
 
-  getActiveTab(newActiveTab) {
-    console.log("newActiveTab: " + newActiveTab);
-		this.setState({
-      activeTab: newActiveTab,
-    });
-    console.log("activeTab: " +  this.state.activeTab);
-	}
+    getActiveTab(newActiveTab) {
+        console.log("newActiveTab: " + newActiveTab);
+        this.setState({
+            activeTab: newActiveTab,
+        });
+        console.log("activeTab: " + this.state.activeTab);
+    }
 
-  getCategories(newCategories) {
-    console.log("newCategories: " + newCategories);
-    this.setState({
-      imageCategory:newCategories[0],
-      textCategory:newCategories[1],
-      soundCategory:newCategories[2],
-    });
-    console.log("categories: " + this.state.imageCategory + ", " + this.state.textCategory  + ", " + this.state.soundCategory);
-  }
+    getCategories(newCategories) {
+        console.log("newCategories: " + newCategories);
+        this.setState({
+            imageCategory: newCategories[0],
+            textCategory: newCategories[1],
+            soundCategory: newCategories[2],
+        });
+        console.log("categories: " + this.state.imageCategory + ", " + this.state.textCategory + ", " + this.state.soundCategory);
+    }
 
-  // Renders gallery text if it exist, if not, shows the welcome text
-  render() {
-    return (
-      <div>
-      <TabComponent getActiveTab={this.getActiveTab} />
-      <CategoryComponent getCategories={this.getCategories} />
+    // Renders gallery text if it exist, if not, shows the welcome text
+    render() {
+        return (
+            <div>
+                <TabComponent getActiveTab={this.getActiveTab} />
+                <CategoryComponent getCategories={this.getCategories} />
 
-      </div>
-      );
-  }
+            </div>
+        );
+    }
 }
 
 export default GalleryComponent;
