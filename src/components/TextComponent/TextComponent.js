c9481ac07a4d70008bf374b09d9332372728ea42
Dabble <ddabble@users.noreply.github.com>
2018-09-20 22:02:21 +0200

    Renamed text files to .json

--- src/components/TextComponent/TextComponent.js
+++ src/components/TextComponent/TextComponent.js
@@ -21,7 +21,7 @@ class TextComponent extends Component {
 
         let category = this.props.category;
         let fileNumber = this.props.fileNumber;
-        let completeUrl = "/media/text/" + category + "/text" + fileNumber + ".js";
+        let completeUrl = "/media/text/" + category + "/text" + fileNumber + ".json";
         fetch(completeUrl)
             .then((response) => response.json())
             .then(data => this.setState({
