26b28a1af800db9b88e973b264d52e3dc4ae303d
Dabble <ddabble@users.noreply.github.com>
2018-09-20 15:36:15 +0200

    Fixed SoundComponent not always loading source
    Also fixed an incorrect filename and added some error checking.
    
    #7

--- src/components/SoundComponent/SoundComponent.js
+++ src/components/SoundComponent/SoundComponent.js
@@ -4,27 +4,15 @@ class SoundComponent extends Component {
     category;
     fileNumber;
 
-    componentDidUpdate() {
-        this.refs.audio.pause();
-        this.refs.audio.load();
-        let playPromise = this.refs.audio.play();
-
-        // Ensures that the above code is not run before the audio is finished preparing to play
-        if (playPromise !== undefined) {
-            playPromise.then(_ => {})
-                .catch(error => {});
-        }
-    }
-
     render() {
-      if(this.props.category===""){
-        return null;
-      }
-      console.log("this.props.category: " + this.props.category + ", this.props.fileNumber: " + this.props.fileNumber);
+        if (this.props.category === "")
+            return null;
+
         return (
             <div className="SoundComponent">
-                <audio controls ref="audio">
-                    <source src={`/media/sound/${this.props.category}/${getFileName(this.props.category, this.props.fileNumber)}`} type="audio/mpeg" />
+                <audio controls autoPlay
+                       src={`/media/sound/${this.props.category}/${getFileName(this.props.category, this.props.fileNumber)}`}
+                       onError={handleAudioError}>
                     Your browser does not support HTML5 audio.
                 </audio>
             </div>
@@ -33,16 +21,22 @@ class SoundComponent extends Component {
 }
 
 const fileDict = {
-    "animals": ["6-cat-meows.mp3", "cat-stevens.mp3", "labrador.mp3", "lion.mp3"],
+    "animals": ["6-cat-meows.mp3", "cat-stevens.mp3", "labrador-barking.mp3", "lion.mp3"],
     "food": ["apple-bite.mp3", "eat-chips.mp3", "smack-lips.mp3", "straw-slurp.mp3"],
     "footsteps": ["footsteps.mp3", "footsteps-on-cement.mp3", "hard-shoes.mp3", "hiking-a-trail.mp3"]
 };
 
 function getFileName(category, fileNumber) {
-    if (category === undefined || category === "")
-        return; // TODO:
+    if (category === undefined || category === "") {
+        console.error("Category was undefined or an empty string!");
+        return;
+    }
 
     return fileDict[category][fileNumber - 1];
+}
+
+function handleAudioError(event) {
+    console.error("Could not find audio source file: " + event.target.src);
 }
 
 export default SoundComponent;
